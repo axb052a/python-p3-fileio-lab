7bab1068dfc35073aa4d4e1fd9bc6e3e47cfd6e8
Anthony Beltran <beltrananthony30@icloud.com>
2023-09-15 10:53:36 -0700

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w') as f: # Add .txt file extension to the file_name
+        f.write(file_content) # Creates a file for content
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content) # Append the content 
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt') as f:
+        return f.read() # Returns the content of the file 
